476cfbb896674f8594552a62ba08358a7213fd6a
Cymen Vig <cymenvig@gmail.com>
2017-03-14 18:10:32 -0500

    makes Subreddit dispatch fetchSubreddit

--- src/components/Subreddit.js
+++ src/components/Subreddit.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import Thing from './Thing';
+import { connect } from 'react-redux';
+import { fetchSubreddit } from '../actions';
 
-export default class Subreddit extends React.Component {
+class Subreddit extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { setSubreddit } = this.props;
+    const { subreddit } = this.props.params;
+
+    setSubreddit(subreddit);
+  }
+
   render() {
     const { subreddit } = this.props.params;
 
     return (
       <div className="subreddit">
         <p>The {subreddit} subreddit!</p>
-        {Array(25).fill().map((_, i) => <Thing />)}
+        {Array(25).fill().map((_, i) => <Thing key={i} />)}
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+  };
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    setSubreddit: (name) => dispatch(fetchSubreddit(name)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Subreddit);
