1c00c52b6b66a196687f13dba3d6bd202120456f
Cymen Vig <cymenvig@gmail.com>
2017-03-14 15:29:10 -0500

    use Header component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+import './index.scss';
+import React from 'react';
+import { render } from 'react-dom';
+import Header from './components/Header';
+
+render((
+  <div>
+    <Header />
+  </div>
+), document.getElementById('root'));
