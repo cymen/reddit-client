f11c1347f70c1b501ddaca022f8ea27eb43843e9
Cymen Vig <cymenvig@gmail.com>
2017-03-14 21:24:31 -0500

    adds subreddits to top of Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,32 @@
 import './Header.scss';
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import classNames from 'classnames';
+
+const categories = [ 'popular', 'all' ];
+const featured = [
+  'AskReddit',
+  'funny',
+  'todayilearned',
+  'gaming',
+  'worldnews',
+  'aww',
+  'pics',
+  'gifs',
+  'videos',
+  'news',
+  'movies',
+  'mildlyinteresting',
+  'showerthoughts',
+  'lifeprotips',
+  'iama',
+  'television',
+  'jokes',
+  'tifu',
+  'WritingPrompts',
+  'space',
+];
 
 class Header extends React.Component {
   render() {
@@ -8,6 +34,25 @@ class Header extends React.Component {
 
     return (
       <div id="header">
+        <div id="sr-header-area">
+          <div className="sr-list">
+            <ul className="flat-list sr-bar hover">
+              {categories.map((category) =>
+                <li key={category} className={classNames({selected: category === activeSubreddit})}>
+                  <Link to={`/r/${category}`}>{category}</Link>
+                </li>
+              )}
+            </ul>
+            <span className="separator">&nbsp;|&nbsp;</span>
+            <ul className="flat-list sr-bar hover">
+              {featured.map((subreddit) =>
+                <li key={subreddit} className={classNames({selected: subreddit === activeSubreddit})}>
+                  <Link to={`/r/${subreddit}`}>{subreddit}</Link>
+                </li>
+              )}
+            </ul>
+          </div> 
+        </div>
         <div id="header-bottom-left">
           <a href="https://reddit.com/" id="header-img" className="default-header">reddit.com</a>
           {activeSubreddit &&
