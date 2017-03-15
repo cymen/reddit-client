5207f8a5b7099b06fc6cce0bf9c1aefc7af4d545
Cymen Vig <cymenvig@gmail.com>
2017-03-15 01:43:11 -0500

    add tests for subreddit reducer

---
+++ test/reducers_spec.js
@@ -0,0 +1,69 @@
+import { expect } from 'chai';
+import timekeeper from 'timekeeper';
+const frozenTime = Date.now();
+timekeeper.freeze(frozenTime);
+import { subreddit } from '../src/reducers';
+import {
+  REQUEST_SUBREDDIT,
+  RECEIVE_SUBREDDIT,
+  ERROR_SUBREDDIT,
+} from '../src/actions';
+
+describe('subreddit reducer', function() {
+  it('should return the initial state', function() {
+    const state = subreddit(undefined, {});
+
+    expect(
+      subreddit(undefined, {})
+    ).to.eql({
+      isFetching: false,
+      lastFetch: frozenTime,
+      name: 'news',
+      view: 'hot',
+    });
+  });
+
+  it('should handle REQUEST_SUBREDDIT', function() {
+    expect(
+      subreddit(undefined, {
+        type: REQUEST_SUBREDDIT,
+        name: 'xyz',
+        view: '42',
+      })
+    ).to.eql({
+      isFetching: true,
+      lastFetch: frozenTime,
+      name: 'xyz',
+      view: '42',
+    });
+  });
+
+  it('should handle RECEIVE_SUBREDDIT', function() {
+    expect(
+      subreddit(
+        {
+          name: 'annarbor',
+          view: 'new'
+        },
+        {
+          type: RECEIVE_SUBREDDIT,
+          name: 'annarbor',
+          view: 'new',
+          data: {
+            children: 'the children are here',
+            after: 'xyz10',
+            before: 'ooo3',
+          },
+        }
+      )
+    ).to.eql({
+      isFetching: false,
+      lastFetch: frozenTime,
+      name: 'annarbor',
+      view: 'new',
+      children: 'the children are here',
+      after: 'xyz10',
+      before: 'ooo3',
+    });
+  });
+}); 
