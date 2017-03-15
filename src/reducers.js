bd1e81806c28dd47dc7f31e8ebdb5118c85a0f94
Cymen Vig <cymenvig@gmail.com>
2017-03-14 22:12:10 -0500

    adds isFetching to subreddit reducer

--- src/reducers.js
+++ src/reducers.js
@@ -5,20 +5,24 @@ import {
 } from './actions';
 
 function subreddit(state = {
+  isFetching: false,
   name: null,
   view: 'hot',
 }, action) {
   switch (action.type) {
     case REQUEST_SUBREDDIT:
       return Object.assign({}, state, {
+        isFetching: true,
         name: action.name,
         view: action.view,
       });
 
     case RECEIVE_SUBREDDIT:
       const { children, after, before } = action.data
+      const isFetching = action.name === state.name && action.view === state.view;
       return Object.assign(
         {
+          isFetching: !isFetching,
           name: action.name,
         },
         state,
