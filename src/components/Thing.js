1b0096dd4969eafe7ca36ebab18de4bab8f35b12
Cymen Vig <cymenvig@gmail.com>
2017-03-14 20:41:19 -0500

    makes Thing render with thumbnails and such as like reddit.com

--- src/components/Thing.js
+++ src/components/Thing.js
@@ -1,17 +1,61 @@
 import './Thing.scss';
 import React from 'react';
+import classNames from 'classnames';
+import isImage from 'is-image';
+import moment from 'moment';
 
 export default class Thing extends React.Component {
+  renderThumbnail = () => {
+    const {
+      thumbnail,
+      url,
+    } = this.props.thing;
+
+    if (thumbnail) {
+      return (
+        <a
+          className={classNames("thumbnail", {
+            default: !isImage(thumbnail),
+          })}
+          href={url}
+        >
+          {isImage(thumbnail) &&
+            <img src={thumbnail} width="70" height="auto" />
+          }
+        </a>
+      );
+    }
+  }
+
+  renderDomain = () => {
+    const { domain } = this.props.thing;
+
+    return (
+      <span className="domain">(<a href={`https://reddit.com/domain/${domain}`}>{domain}</a>)</span>
+    );
+  }
+
   render() {
+    const {
+      author,
+      created_utc,
+      num_comments,
+      permalink,
+      title,
+    } = this.props.thing;
+
     return (
       <div className="thing">
-        <p className="title"><a href="#" target="_blank">Title</a></p>
-        <p className="tagline">submitted ...</p>
-        <ul className="flat-list buttons">
-          <li>
-            <a href="#">42 comments</a>
-          </li>
-        </ul>
+        {this.renderThumbnail()}
+        <div className="entry">
+          <p className="title"><a href="#" target="_blank">{title}</a> {this.renderDomain()}</p>
+          <p className="tagline">submitted {moment(created_utc * 1000).from()} by {author}</p>
+          <ul className="flat-list buttons">
+            <li>
+              <a href={`https://www.reddit.com${permalink}`} target="_blank">{num_comments} comments</a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
